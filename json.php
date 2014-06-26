a14f907839cc262479e4b968d53f34dc50b88ce4
Yaroslav Snisar <info@rustem.com.ua>
2014-06-26 11:49:25 +0300

    fix sql-injection

--- json.php
+++ json.php
@@ -2,7 +2,7 @@
 include("functions.inc.php");
 
 if (isset($_GET['posada'])) {
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT id, name FROM posada;";
     $result = mysql_query($qstring);//query the database for entries containing the term
@@ -30,7 +30,7 @@ if (isset($_GET['posada'])) {
 
 
 if (isset($_GET['units'])) {
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT id, name FROM units;";
     $result = mysql_query($qstring);//query the database for entries containing the term
@@ -62,7 +62,7 @@ if (isset($_GET['units'])) {
 
 if (isset($_GET['users_do'])) {
 
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT fio as label, id as value FROM users WHERE fio LIKE '%".$term."%' limit 10";
     $result = mysql_query($qstring);//query the database for entries containing the term
@@ -83,7 +83,7 @@ if (isset($_GET['users_do'])) {
 
 
 if (isset($_GET['pod'])) {
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT name as label, id as value FROM units WHERE name LIKE '%".$term."%' limit 10";
     $result = mysql_query($qstring);//query the database for entries containing the term
@@ -101,7 +101,7 @@ if (isset($_GET['pod'])) {
 }
 
 if (isset($_GET['fio'])) {
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT fio as label, login as label2, tel as label3, unit_desc as label4, id as value FROM clients WHERE (fio LIKE '%".$term."%') or (login LIKE '%".$term."%') or (tel LIKE '%".$term."%') limit 10";
     $result = mysql_query($qstring);//query the database for entries containing the term
@@ -123,7 +123,7 @@ if (isset($_GET['fio'])) {
 
 
 if (isset($_GET['login'])) {
-    $term = trim(strip_tags($_GET['term']));//retrieve the search term that autocomplete sends
+    $term = trim(strip_tags(mysql_real_escape_string($_GET['term'])));//retrieve the search term that autocomplete sends
 
     $qstring = "SELECT login as label, fio as label2, tel as label3, unit_desc as label4, id as value FROM clients WHERE login LIKE '%".$term."%' limit 10";
     $result = mysql_query($qstring);//query the database for entries containing the term
