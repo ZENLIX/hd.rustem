7fe2f3471f5467dc8c8143bb7f3867b912547405
Yaroslav Snisar <info@rustem.com.ua>
2014-07-02 11:59:15 +0300

    move some client_info functions to main actions file.

--- actions.php
+++ actions.php
@@ -227,6 +227,93 @@ if (mysql_num_rows($res) == 0) {
 
 
 
+if ($mode == "get_client_from_new_t") {
+if (isset($_POST['get_client_info'])) {
+
+    $client_id=mysql_real_escape_string($_POST['get_client_info']);
+
+
+
+    //include("functions.inc.php");
+    get_client_info($client_id);
+
+
+
+}
+if (isset($_POST['new_client_info'])) {
+    $fio=mysql_real_escape_string($_POST['new_client_info']);
+    $u_l=mysql_real_escape_string($_POST['new_client_login']);
+    //include("functions.inc.php");
+    ?>
+
+
+    <div id="" class="alert alert-warning alert-dismissable" style="padding: 5px; margin-bottom: 10px;">
+        <button style="right: 0px;" type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <small>
+            <?=lang('msg_created_new_user');?> <br></small>
+    </div>
+    <div class="panel panel-success" id="user_info" style="display: block;">
+        <div class="panel-body">
+            <div class="panel-heading">
+                <h4 class="panel-title"><i class="fa fa-user"></i> <?=lang('WORKER_TITLE');?></h4>
+            </div>
+            <div class="panel-body">
+
+
+
+
+                <table class="table  ">
+                    <tbody>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_fio');?>:</small></td>
+                        <td><small>
+                                <a href="#" id="username" data-type="text" data-pk="1" data-title="Enter username"><?=$fio?></a>
+                            </small>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_login');?>:</small></td>
+                        <td><small><a href="#" id="new_login" data-type="text"  data-pk="1" data-title="Enter username"><?=$u_l?></a></small></td>
+                    </tr>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_posada');?>:</small></td>
+                        <td><small><a href="#" id="new_posada"  data-type="select" data-source="<?=$CONF['hostname'];?>/json.php?posada" data-pk="1" data-title="<?=lang('WORKER_posada');?>"></a></small></td>
+                    </tr>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_unit');?>:</small></td>
+                        <td><small><a href="#" id="new_unit" data-type="select" data-source="<?=$CONF['hostname'];?>/json.php?units" data-pk="1" data-title="<?=lang('NEW_unit');?>"></a></small></td>
+                    </tr>
+
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_tel');?>:</small></td>
+                        <td><small><a href="#" id="new_tel" data-type="text" data-pk="1" data-title="Enter username"></a></small></td>
+                    </tr>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_room');?>:</small></td>
+                        <td><small><a href="#" id="new_adr" data-type="text" data-pk="1" data-title="Enter username"></a></small></td>
+                    </tr>
+                    <tr>
+                        <td style=" width: 30px; "><small><?=lang('WORKER_mail');?>:</small></td>
+                        <td><small><a href="#" id="new_mail" data-type="text" data-pk="1" data-title="Enter username"></a></small></td>
+                    </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+<?php
+}
+
+}
+
+
+
+
+
+
+
         if ($mode == "get_report") {
             $id=mysql_real_escape_string($_POST['id']);
             $s=mysql_real_escape_string($_POST['s']);
@@ -683,6 +770,10 @@ print json_encode($results);
         $qstring = "SELECT fio, id, unit FROM users;";
         $result = mysql_query($qstring);//query the database for entries containing the term
 		//while ($row = mysql_fetch_array($result,MYSQL_ASSOC))//loop through the retrieved values
+		$results[] = array(
+                'name' => '',
+                'co' => '0'
+            );
 		while ($row = mysql_fetch_assoc($result)) 
         {
         $un=$row['fio'];
@@ -822,7 +913,7 @@ if ($val== $row['value']) {$opt_sel="selected";}
     <div class="form-group">
         <label for="u" class="col-md-2 control-label"><small><?=lang('NEW_to');?>: </small></label>
         <div class="col-md-6">
-            <select data-placeholder="Підрозділ" class="chosen-select form-control" id="u" name="unit_id" multiple>
+            <select style="height: 34px;" data-placeholder="<?=lang('NEW_to_unit');?>" class="chosen-select form-control" id="u" name="unit_id" multiple>
                 <option value="0"><?=lang('HELP_all');?></option>
                 <?php
                         $qstring = "SELECT name as label, id as value FROM deps where id !='0' ;";
@@ -983,7 +1074,16 @@ $results = mysql_query("SELECT
 							from helper
 							order by dt desc
 							");
-
+if(mysql_num_rows($results)==0) {
+?>
+<div id="" class="well well-large well-transparent lead">
+                <center>
+                    <?=lang('MSG_no_records');?>
+                </center>
+            </div>
+<?php
+}
+if(mysql_num_rows($results)>0) {
         while ($row = mysql_fetch_assoc($results)) {
         
         $unit2id = explode(",", $row['unit_to_id']);
@@ -1038,6 +1138,7 @@ $results = mysql_query("SELECT
 	        
         </p>
         <?php
+        }
         }
         }
         
