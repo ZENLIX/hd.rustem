e260b0a50fa9f3d35f5a9b267432ac59d923d971
dima-bzz <dimabzz@gmail.com>
2014-07-16 18:06:43 +0400

    fix

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -74,7 +74,7 @@ if ($newt == 0) {
                 <ul class="dropdown-menu">
                     <!--li <?=echoActiveClassIfRequestMatches("clients.php")?> ><a href="clients.php"><i class="fa fa-users"></i> Клієнти</a></li-->
                     <li <?=echoActiveClassIfRequestMatches("users")?>><a href="<?=$CONF['hostname']?>users"><i class="fa fa-users"></i> <?=lang('NAVBAR_users');?></a></li>
-                    <li <?=echoActiveClassIfRequestMatches("deps")?>><a href="<?=$CONF['hostname']?>/deps"><i class="fa fa-sitemap"></i> <?=lang('NAVBAR_deps');?></a></li>
+                    <li <?=echoActiveClassIfRequestMatches("deps")?>><a href="<?=$CONF['hostname']?>deps"><i class="fa fa-sitemap"></i> <?=lang('NAVBAR_deps');?></a></li>
                     <li <?=echoActiveClassIfRequestMatches("approve")?>><a href="<?=$CONF['hostname']?>approve"><i class="fa fa-check-square-o"></i> <?=lang('NAVBAR_approve');?></a></li>
                     
                     <li class="divider"></li>
