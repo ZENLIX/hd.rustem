dffc5440b41c09778685c5a33f75f6bf5ffa3281
dima-bzz <dimabzz@gmail.com>
2014-07-17 12:45:48 +0400

    tr

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -463,6 +463,7 @@ function lang($phrase){
 'TICKET_file_list'=>'Прикреплённые файлы',
 'SYSTEM_lang'=>'Язык',
 'summernote_lang'=> 'ru-RU',
+'note_save'=>'Запись сохранена',
 ''=>''
 
             );
