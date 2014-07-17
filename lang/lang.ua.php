001eb3a4710ced418ad6453b8d1da8ffcdf493ba
dima-bzz <dimabzz@gmail.com>
2014-07-17 12:46:22 +0400

    Translate note

--- lang/lang.ua.php
+++ lang/lang.ua.php
@@ -411,6 +411,7 @@ function lang($phrase){
 'TICKET_file_list'=>'Додатковi файли',
 'SYSTEM_lang'=>'Мова',
 'summernote_lang'=> 'uk-UA',
+'note_save'=>'Запись збережена',
 '' => '',
 '' => '',
 '' => '',
