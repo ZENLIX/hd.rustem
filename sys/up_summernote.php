d73f68b8236f925a222c351013d6b80e6e8da4ea
Yaroslav Snisar <info@rustem.com.ua>
2014-09-23 09:09:28 +0300

    fix #251.

--- sys/up_summernote.php
+++ sys/up_summernote.php
@@ -2,7 +2,7 @@
 include_once("../functions.inc.php");
 if ($_FILES['file']['name']) {
             if (!$_FILES['file']['error']) {
-                $name = md5(rand(100, 200));
+                $name = md5(time());
                 $ext = explode('.', $_FILES['file']['name']);
                 $filename = $name . '.' . $ext[1];
                 $destination = realpath(dirname(dirname(__FILE__)))."/upload_files/user_content/" . $filename; //change this directory
